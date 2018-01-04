cc25828bf456f90cd407445662673727c8f51fa5
vFlav <flv691@mail.usask.ca>
2018-01-03 21:58:06 -0600

    Did not save file while editting, missed 2 commits. Added deltaSet method that build the set returned by the delta function. Added delta method that checks current element against provided fragments. Compiles.

--- pdp.java
+++ pdp.java
@@ -15,7 +15,7 @@ import java.util.*;
 
 public class pdp{
 	
-	//need a pair class to store pairs of value n and d
+	//Need a pair class to store pairs of value n and d;
 	//this should not cause problems, the class is small
 	public class pair{
 		final int n;
@@ -28,7 +28,7 @@ public class pdp{
 		}
 	}
 	
-	//scanner for input, L to keep track of provided elements, X is the solution list
+	//Scanner for input, L to keep track of provided elements, X is the solution list
 	//and S is the stack keeping track of pairs
 	Scanner 			scan;
 	LinkedList<Integer> L;
@@ -41,6 +41,48 @@ public class pdp{
 		L	 = new LinkedList<>();
 		X 	 = new LinkedList<>();
 		S	 = new Stack<>();
+	}
+	
+	//Scan input and add given digest fragments to L
+	public void readFragments(){
+		int num_el = scan.nextInt();
+		int next;
+		
+		for(int i = 0; i < num_el; i++){
+			next = scan.nextInt();
+			this.L.add(next);
+		}
+	}
+	
+	//Return the set create by the delta function
+	//el - the current fragment passed to the delta function
+	public LinkedList<Integer> deltaSet(int el){
+		LinkedList<Integer> new_list = new LinkedList<>();
+		
+		//walk through X and build new_list
+		for(int i : X){
+			if(i != el){
+				new_list.add(Math.abs(el - i));
+			}
+		}
+		
+		return new_list;
+	}
+	
+	//Return true if the set returned by delta is a subset of L
+	public boolean delta(int el, LinkedList<Integer> X){
+		//build a new list that's the result of the delta function
+		LinkedList<Integer> new_list = this.deltaSet(el);
+		
+		//check for subset
+		for(int i : new_list){
+			if(!this.L.contains(i)){
+				return false;
+			}
+		}
+		
+		//a subset
+		return true;
 	}
 	
 	//main method to run everything 
