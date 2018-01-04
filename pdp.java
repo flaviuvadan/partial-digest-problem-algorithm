ad0f3ab97fc2df17fdcc127ba301b3d9d6a46141
vFlav <flv691@mail.usask.ca>
2018-01-03 21:43:27 -0600

    Started creating the pdp class. Also added a small pair class.

---
+++ pdp.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+/*
+* A simple Java implementation of the partial digest problem backtracking algorithm.
+*
+* Instructions: compile using javac pdp.java
+* Input format: <num elements>
+*				<num1>
+*				<num2>
+*				...
+*				<num n>
+* Output format: X: [elements]
+*
+*/
+
+public class pdp{
+	
+	//need a pair class to store pairs of value n and d
+	//this should not cause problems, the class is small
+	public class pair{
+		final int n;
+		final int d;
+		
+		//constructor to initialize values
+		pair(int num, int dval){
+			this.n = num;
+			this.d = dval;
+		}
+	}
+	
+	//scanner for input, L to keep track of provided elements, X is the solution list
+	//and S is the stack keeping track of pairs
+	Scanner 			scan;
+	LinkedList<Integer> L;
+	LinkedList<Integer> X;
+	Stack<Pair>			S;
+	
+	//constructor to initialize a pdp class
+	pdp(){
+		scan = new Scanner(System.in);
+		L	 = new LinkedList<>();
+		X 	 = new LinkedList<>();
+		S	 = new Stack<>();
+	}
+}
+	
+	
+	
